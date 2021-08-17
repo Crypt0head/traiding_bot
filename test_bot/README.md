3c1f5febcf759d90f0e5c09747215d2c04a8c8a6
Crypt0head <zelas.money@yandex.ru>
2021-08-18 01:17:18 +0300

    Add linux setup script to install all py deps

--- test_bot/README.md
+++ test_bot/README.md
@@ -1,6 +1,17 @@
 ***TEST TRAIDING BOT***
 
-This is a simple trading bot for general purpose 
+This is a simple trading bot for general purpose
+
+***Get Python dependencies***
+
+**For Linux**
+
+    sudo chmod +x setup_linux.sh
+    ./setup_linux.sh
+
+**For Windows**
+
+    TBA
 
 ***HOW TO BUILD***
 
---
+++ test_bot/setup_linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo 'Setting up project dependencies'
+
+echo 'Getting TA-lib source:'
+
+sudo wget -P /tmp http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz 
+
+sudo tar -xv -C /tmp -f /tmp/ta-lib-0.4.0-src.tar.gz
+
+cd /tmp/ta-lib
+
+echo 'Configure installation dir:'
+
+./configure --prefix=/usr
+
+echo 'Building and Installing TA-lib:'
+
+make || sudo make install 
+
+echo 'Installing python-dependencies from requirements file:'
+
+cd -
+
+python3 -m pip install -r requirements.txt
+
+echo 'Clean Up'
+
+sudo rm -r /tmp/ta-lib-0.4.0-src.tar.gz /tmp/ta-lib
